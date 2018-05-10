c501a37ddfccd2ef34e2e547c6a8b699b082200d
kim yong-jae <kyj787@naver.com>
2018-05-10 14:01:10 +0900

    todo 사이드메뉴, dao,dto,service

--- src/semi/teamP/dao/TodoDAO.java
+++ src/semi/teamP/dao/TodoDAO.java
@@ -4,10 +4,13 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
+
+import semi.teamP.dto.TodoDTO;
 
 public class TodoDAO {
 	Connection conn = null;
@@ -35,5 +38,67 @@ public class TodoDAO {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+
+
+	public int write(TodoDTO dto) {
+		int success=0;
+		
+		String sql = "INSERT INTO Todo(todo_idx,todo_content,member_id)"
+				+"VALUES(todo_idx_seq.NEXTVAL,?,?)";
+			try {
+				ps=conn.prepareStatement(sql);
+				ps.setString(1, dto.getTodo_content());
+				ps.setString(2, dto.getMember_id());
+				success=ps.executeUpdate();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}finally {
+				resClose();
+			}
+		
+		return success;
+	}
+
+
+	public ArrayList<TodoDTO> list(String loginId) {
+		ArrayList<TodoDTO> list = new ArrayList<TodoDTO>(); //list변수만들어주고
+		String sql="SELECT todo_content,member_id FROM Todo WHERE member_id=? ORDER BY todo_idx DESC";
+		try {
+			ps=conn.prepareStatement(sql);
+			ps.setString(1, loginId);
+			rs=ps.executeQuery();
+			while(rs.next()){ 
+				TodoDTO dto = new TodoDTO();	
+				dto.setTodo_content(rs.getString("todo_content"));
+				dto.setMember_id(rs.getString("member_id"));
+				list.add(dto);//dto 를 list 에 담기
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			resClose();
+		}
+		return list;
+	}
+
+
+	public int delete(String[] delList) {
+		String sql="DELETE FROM Todo WHERE todo_idx=?";
+		int delCnt=0;
+		try {
+			for(int i=0; i<delList.length; i++) {
+				ps=conn.prepareStatement(sql);
+				ps.setInt(1,Integer.parseInt(delList[i]));
+				delCnt += ps.executeUpdate();
+				ps.close();	
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			resClose();
+		}
+
+		return delCnt;
 	}
 }
